fd3ba790ad7e4d2ed1e711318a9a8b93d5ee2f3a
Haruka11 <ushiromiya3@gmail.com>
2016-08-31 17:12:57 +0900

    Toriaezu kokode hozon

--- app/src/main/java/com/example/kohki/withmanager/Synchro.java
+++ app/src/main/java/com/example/kohki/withmanager/Synchro.java
@@ -1,21 +1,39 @@
 package com.example.kohki.withmanager;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.io.OutputStream;
+import java.util.Set;
+
 public class Synchro extends AppCompatActivity {
+    private BluetoothAdapter mBluetoothAdapter;
+    private BluetoothDevice mBtDevice;
+    private BluetoothSocket mBtsocket;
+    private OutputStream mOutput;
+
     Button btnStart;
     boolean isMain;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_synchro);
+
+
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
@@ -38,7 +56,13 @@ public class Synchro extends AppCompatActivity {
                 isMain = false;
                 break;
         }
-
         return super.onOptionsItemSelected(item);
     }
+
+    private final View.OnClickListener btn_startClicked = new View.OnClickListener(){
+        @Override
+        public void onClick(View view){
+//            Intent itt_start = new Intent(getApplication(), );
+        }
+    };
 }
